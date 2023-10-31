db25cc8363dc32f6a6b13bb135ccd505db5b8341
joaocunha <jvscunha04@gmail.com>
2023-10-31 18:31:33 -0300

    estruturando o app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LocationsComponent } from './pages/locations/locations.component';
+import { EpisodesComponent } from './pages/episodes/episodes.component';
+import { MenuBarComponent } from './componentes/menu-bar/menu-bar.component';
+import { FiltersComponent } from './componentes/filters/filters.component';
+import { CharsComponent } from './componentes/chars/chars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LocationsComponent,
+    EpisodesComponent,
+    MenuBarComponent,
+    FiltersComponent,
+    CharsComponent
   ],
   imports: [
     BrowserModule,
