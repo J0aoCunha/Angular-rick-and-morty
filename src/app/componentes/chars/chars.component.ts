ddd6229b6b51d5e6d08bcb7e8a202dd78262f03f
joaocunha <jvscunha04@gmail.com>
2023-10-31 18:54:41 -0300

    consertando a estrutura da home page

--- src/app/componentes/chars/chars.component.ts
+++ src/app/componentes/chars/chars.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharsComponent implements OnInit{
 
+imageChars: string = '../../../assets/rick.png';
+nameChars: string = 'Rick';
+speciesChars: string = 'Human';
+
+
   constructor() {}
 
   ngOnInit(): void {}
