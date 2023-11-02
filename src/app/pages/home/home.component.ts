c67cb08916caba2d89862d414716271a70833841
joaocunha <jvscunha04@gmail.com>
2023-11-02 20:17:58 -0300

    update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,36 +2,33 @@ import { Component } from '@angular/core';
 import { Char } from 'src/app/models/chars.model';
 import { ApirickandmortyService } from '../../services/apirickandmorty.service';
 
-
 @Component({
-	selector: 'app-home',
-	templateUrl: './home.component.html',
-	styleUrls: ['./home.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
-
 export class HomeComponent {
+  char: Char[] = [];
+  page: number = 1;
 
+  constructor(private apirickandmortyService: ApirickandmortyService) {
+    this.getChars();
+  }
 
-	char: Char[] = [];
-	page: number = 1;
+  getChars() {
+    this.apirickandmortyService
+      .obterPersonagens(this.page)
+      .subscribe((response) => {
+        this.char = response.results;
+      });
+  }
 
-	constructor(private apirickandmortyService: ApirickandmortyService) {
-		this.getChars();
-	}
-
-	getChars() {
-		this.apirickandmortyService.obterPersonagens(this.page).subscribe(response => {
-			this.char = response.results;
-		});
-	}
-
-	loadMore() {
-		this.apirickandmortyService.obterPersonagens(this.page + 1).subscribe(response => {
-			this.char = [...this.char, ...response.results];
-			this.page++;
-		});
-	}
-	
+  loadMore() {
+    this.apirickandmortyService
+      .obterPersonagens(this.page + 1)
+      .subscribe((response) => {
+        this.char = [...this.char, ...response.results];
+        this.page++;
+      });
+  }
 }
-
-
