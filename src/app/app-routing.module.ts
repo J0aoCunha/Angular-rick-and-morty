c7d569f882f3b1d499a96498946fd440030c1870
joaocunha <jvscunha04@gmail.com>
2023-11-02 21:36:57 -0300

    criando rotas da aplicaçao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import {ContentcharsComponent } from './pages/contentchars/contentchars.component';
+import { ContentcharsComponent } from './pages/contentchars/contentchars.component';
+import { LocationsComponent } from './pages/locations/locations.component';
+import { EpisodesComponent } from './pages/episodes/episodes.component';
 
 const routes: Routes = [
-
-	{
-		path: '',
-		component: HomeComponent
-	 },
-	 {
-		path: 'contentchars/:id',
-		component: ContentcharsComponent
-	  }
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'contentchars/:id',
+    component: ContentcharsComponent,
+  },
+  {
+    path: 'locations',
+    component: LocationsComponent,
+  },
+  {
+    path: 'episodes',
+    component: EpisodesComponent,
+  },
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
