b515b9b7ad89ca913675a38f411ab1c3cc4727a7
joaocunha <jvscunha04@gmail.com>
2023-11-03 20:50:26 -0300

    criando e estilizando o card de planetas

---
+++ src/app/componentes/card-location/card-location.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-location',
+  templateUrl: './card-location.component.html',
+  styleUrls: ['./card-location.component.css'],
+})
+export class CardLocationComponent {
+  @Input() name: string = '';
+  @Input() type: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
