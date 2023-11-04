7f4dfc105c2072f902759eb76034124f1b2a09e1
joaocunha <jvscunha04@gmail.com>
2023-11-04 01:35:52 -0300

    criando e modificando o componente  button-more

---
+++ src/app/componentes/button-more/button-more.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-more',
+  templateUrl: './button-more.component.html',
+  styleUrls: ['./button-more.component.css']
+})
+export class ButtonMoreComponent {
+
+}
