951cf487d94fbbe051b2b8a98fa914365ba76daf
joaocunha <jvscunha04@gmail.com>
2023-11-09 22:55:26 -0300

    deixando a pagina dinamica

--- src/app/pages/content-episodes/content-episodes.component.ts
+++ src/app/pages/content-episodes/content-episodes.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Episode } from 'src/app/models/episodes.model';
+import { ApirickandmortyService } from 'src/app/services/apirickandmorty.service';
+import { EpisodesService } from 'src/app/services/episodes.service';
 
 @Component({
   selector: 'app-content-episodes',
   templateUrl: './content-episodes.component.html',
-  styleUrls: ['./content-episodes.component.css']
+  styleUrls: ['./content-episodes.component.css'],
 })
 export class ContentEpisodesComponent {
+  nameEpisode: string = '';
+  EpisodesSeason: string = '';
+  Date: string = '';
+  private id: number = 0;
 
+  Cast: any[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private episodesService: EpisodesService,
+    private apirickandmortyService: ApirickandmortyService,
+  ) {}
+
+  ngOnInit(): void {
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+    this.episodesService.getEpisodeDetails(this.id).subscribe((episode) => {
+      this.nameEpisode = episode.name;
+      this.EpisodesSeason = episode.episode;
+      this.Date = episode.air_date;
+
+      for (const CastUrl of episode.characters) {
+        this.apirickandmortyService
+          .getCharacterDetailsByUrl(CastUrl)
+          .subscribe((character) => {
+            this.Cast.push(character);
+          });
+      }
+    });
+  }
 }
--- src/app/services/episodes.service.ts
+++ src/app/services/episodes.service.ts
@@ -3,14 +3,21 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { EpisdesResponse } from '../models/Episodesresponse.model';
+import { Episode } from '../models/episodes.model';
 @Injectable({
   providedIn: 'root',
 })
 export class EpisodesService {
-  private linkApi = 'https://rickandmortyapi.com/api/episode/?page=';
+  private linkApi = 'https://rickandmortyapi.com/api/episode/';
   constructor(private httpClient: HttpClient) {}
 
   obterEpisodes(page: number): Observable<EpisdesResponse> {
-    return this.httpClient.get<EpisdesResponse>(`${this.linkApi}${page}`);
+    return this.httpClient.get<EpisdesResponse>(
+      `${this.linkApi}/?page=${page}`,
+    );
+  }
+
+  getEpisodeDetails(id: number): Observable<Episode> {
+    return this.httpClient.get<Episode>(`${this.linkApi}${id}`);
   }
 }
